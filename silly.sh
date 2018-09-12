6a11cb89dd877bb62cd8c8953eebbd9ee30ee350
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-09-11 23:11:52 -0400

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "This is a silly script" > /tmp/silly.txt
+echo "This is a silly apache install" > /tmp/silly.txt
 
-sudo yum update - y
-yum install httpd -y
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl restart httpd.service
